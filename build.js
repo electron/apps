b35d4a7d842ea94a50f937b1b535ae6a68be62e0
Zeke Sikelianos <zeke@sikelianos.com>
2016-12-08 16:11:40 -0800

    refactor like crazy

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
+const fs = require('fs')
 const path = require('path')
-const Datauri = require('datauri').sync
 const yaml = require('yamljs')
+const slugg = require('slugg')
+const mkdirp = require('mkdirp')
 const blacklist = [
   'Electron Packager',
   'Electron Builder',
@@ -11,10 +13,19 @@ const blacklist = [
 ]
 
 const apps = yaml.load('./apps.yml')
-  .filter(app => blacklist.indexOf(app.name) === -1)
-  .map(app => {
-    app.datauri = Datauri(path.join(__dirname, `/icons/${app.icon}`))
-    return app
+  .filter(app => !blacklist.includes(app.name))
+  .forEach(app => {
+    const slug = slugg(app.name)
+    const basedir = path.join(__dirname, `/apps/${slug}`)
+    const ext = path.extname(app.icon)
+    mkdirp(basedir)
+    // fs.renameSync(
+    //   path.join(__dirname, `/icons/${app.icon}`),
+    //   path.join(basedir, `${slug}${ext}`)
+    // )
+    delete app.icon
+    fs.writeFileSync(
+      path.join(basedir, `${slug}.yml`),
+      yaml.stringify(app, 2)
+    )
   })
-
-process.stdout.write(JSON.stringify(apps, null, 2))
--- test.js
+++ test.js
@@ -1,22 +1,58 @@
-const test = require('tape')
+const fs = require('fs')
 const path = require('path')
-const exists = require('path-exists').sync
-const apps = require('.')
+const expect = require('chai').expect
+const pathExists = require('path-exists').sync
+const yaml = require('yamljs')
+const isUrl = require('is-url')
+const cleanDeep = require('clean-deep')
+const slugg = require('slugg')
+const slugs = fs.readdirSync(path.join(__dirname, '/apps'))
 
-test('apps', function (t) {
-  t.ok(Array.isArray(apps), 'is an array')
-  t.ok(apps.length > 80, 'has lots of apps in it')
+describe('electron-apps', () => {
 
-  apps.forEach(app => {
-    t.comment(app.name)
-    t.ok(app.name.length > 0, 'has a name')
-    const iconPath = path.join(__dirname, `/icons/${app.icon}`)
-    t.ok(
-      app.icon && exists(iconPath),
-      'has an icon file'
-    )
-    t.ok(app.datauri.length > 0, 'has a datauri')
+  it('includes lots of apps', () => {
+    expect(slugs.length).to.be.above(80)
   })
 
-  t.end()
+  slugs.forEach(slug => {
+    describe(slug, () => {
+      const basedir = path.join(__dirname, `/apps/${slug}`)
+      const yamlFile = `${slug}.yml`
+      const yamlPath = path.join(basedir, yamlFile)
+      const pngPath = path.join(basedir, `${slug}.png`)
+      const svgPath = path.join(basedir, `${slug}.svg`)
+
+      it('is in a directory whose name is lowercase with dashes as a delimiter', () => {
+        expect(slugg(slug)).to.equal(slug)
+      })
+
+      it(`includes a data file named ${slug}.yml`, () => {
+        expect(pathExists(yamlPath)).to.equal(true)
+      })
+
+      it(`includes an icon named named ${slug}.png or ${slug}.svg`, () => {
+        expect(pathExists(pngPath) || pathExists(svgPath)).to.equal(true)
+      })
+
+      describe(`${yamlFile}`, () => {
+        const app = yaml.load(yamlPath)
+
+        it('has a name', () => {
+          expect(app.name).to.not.be.empty
+        })
+
+        it('has a description', () => {
+          expect(app.description).to.not.be.empty
+        })
+
+        it('has a valid repository URL (or no repository)', () => {
+          expect(!app.repository || isUrl(app.repository)).to.equal(true)
+        })
+
+        it('has no empty properties', () => {
+          expect(cleanDeep(app)).to.deep.equal(app)
+        })
+      })
+    })
+  })
 })
