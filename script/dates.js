508fa277ccbcc3703d028d48ca77bff8f3a121d1
Zeke Sikelianos <zeke@sikelianos.com>
2017-02-08 22:48:15 -0800

    extract app submission dates from git history

---
+++ script/dates.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const yaml = require('yamljs')
+const execSync = require('child_process').execSync
+const datesPath = path.join(__dirname, '../dates.json')
+const dates = require(datesPath)
+const existingSlugs = Object.keys(dates)
+
+console.log('Checking app submission dates...')
+
+fs.readdirSync(path.join(__dirname, '../apps'))
+  .filter(filename => {
+    return fs.statSync(path.join(__dirname, `../apps/${filename}`)).isDirectory()
+  })
+  .filter(slug => !existingSlugs.includes(slug))
+  .forEach(slug => {
+    const yamlFile = path.join(__dirname, `../apps/${slug}/${slug}.yml`)
+    const app = yaml.load(yamlFile)
+    cmd = `git log -S "${app.website}" --pretty=format:'%cI' | tail -n1`
+    var date = String(execSync(cmd)).slice(0, 10)
+    console.log(`${slug}: ${date}`)
+    dates[slug] = date
+  })
+
+fs.writeFileSync(datesPath, JSON.stringify(dates, null, 2))
