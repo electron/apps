64c96c21d3a6a5ee9d4285aec4ba28b2d380b76a
Zeke Sikelianos <zeke@sikelianos.com>
2017-08-10 12:28:28 -0700

    standardize

--- script/dates.js
+++ script/dates.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const execSync = require('child_process').execSync
 const datesPath = path.join(__dirname, '../meta/dates.json')
 const dates = require(datesPath)
 const existingSlugs = Object.keys(dates)
@@ -11,7 +10,7 @@ console.log('Checking app submission dates...')
 apps
   .filter(app => existingSlugs.indexOf(app.slug) === -1)
   .forEach(app => {
-    const date = new Date().toISOString().slice(0,10)
+    const date = new Date().toISOString().slice(0, 10)
     console.log(`${app.slug}: ${date}`)
     dates[app.slug] = date
   })
