fb971b282cf5e1c8d44958a1db8a84e8e0068898
Michelle Tilley <michelle@michelletilley.net>
2019-06-03 09:45:36 -0700

    Maybe fix tests??

--- test/colors-spec.js
+++ test/colors-spec.js
@@ -115,9 +115,9 @@ describe('colors', function () {
 
     const colors = await Colors.getColors(input, {}, testDir)
     colors.should
-      .have.keys(goodEntry.slug)
+      .include.keys(goodEntry.slug)
       .and
-      .not.have.keys(badEntry.slug)
+      .not.include.keys(badEntry.slug)
 
     expect(consoleError.callCount).to.equal(1)
     expect(consoleError.firstCall.args[0]).to.include(badEntry.iconPath)
