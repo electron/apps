eb5f07b654e95501000609d0ac57a9d0ff962612
Charles Kerr <ckerr@github.com>
2019-06-29 17:53:30 -0500

    check for broken non-ssl links too

--- script/find-broken-links.js
+++ script/find-broken-links.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 /* Links can break at any time and it's outside of the repo's control,
-   so it doesn't make sense to put this into CI. Instead, this should
-   be periodically as part of a separate periodic process. */
+   so it doesn't make sense to run this script as part of CI. Instead,
+   this should be run periodically as part of a separate process. */
 
 /* TODO: should this do anything corrective as well?
    e.g. if all the links in a file are dead, disable the file? */
@@ -16,26 +16,27 @@ const yaml = require('yamljs')
 
 // walk an object subtree looking for https:// strings
 const getObjectUrls = root => {
+  const urlPrefixes = ['http://', 'https://']
   const urls = []
   const queue = [ root ]
   while (queue.length !== 0) {
     const vals = Object.values(queue.shift())
-    urls.push(...vals.filter(v => typeof v === 'string' && v.startsWith('https://')))
+    urls.push(...vals.filter(v => typeof v === 'string' && urlPrefixes.some(prefix => v.startsWith(prefix))))
     queue.push(...vals.filter(v => typeof v === 'object'))
   }
   return urls
 }
 
 // scrape a url to see if the link is broken.
-// returns a promise that resolves with { url, err }
+// return a promise that resolves with { url, err }
 const scrape = url =>
-  fetch(url, { method: 'HEAD' })
-    .then(res => { return { url, err: res.ok ? null : `${res.status} ${res.statusText}` } })
-    .catch(err => { return { url, err } })
+  fetch(url, { method: 'HEAD' }) // just scrape the headers; body not needed
+    .then(res => ({ url, err: res.ok ? null : `${res.status} ${res.statusText}` }),
+          err => ({ url, err }))
 
 // scrape all the urls found in a yml file.
-// reports broken links to console.log().
-// returns a promise that resolves with an array of broken links: { url, err }
+// report broken links to console.log().
+// return a promise that resolves with an array of broken links: { url, err }
 const processYmlEntry = entry =>
   fsPromises.readFile(entry.fullPath, { encoding: 'utf8' })
     .then(yaml.parse)
@@ -47,4 +48,5 @@ const processYmlEntry = entry =>
 const topDir = path.join(path.dirname(__dirname))
 readdirp.promise(topDir, { fileFilter: '*.yml', directoryFilter: entry => entry.path.startsWith('apps') })
   .then(entries => Promise.all(entries.map(processYmlEntry)))
-  .then(fails => process.exit(fails.flat().length))
+  .then(arraysOfFails => arraysOfFails.flat())
+  .then(fails => process.exit(fails.length))
